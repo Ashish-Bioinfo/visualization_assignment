1e42cc54861b6224b092c092d2b33e0959e7d802
Ashish-Bioinfo <ashish_tomar87@yahoo.in>
2015-04-27 14:00:00 +0530

    updated

--- csv2json.py
+++ csv2json.py
@@ -2,11 +2,12 @@
 from optparse import OptionParser
 import csv
 import json
-
+import itertools
 class CSV2JSON(object):
 
   def __init__(self):
-   
+
+    self.header = ""   
     (self.csv_file , self.json_file) = self.getOpt()
     if  self.json_file == None:
          self.json_file = self.csv_file.replace('.csv','.json')
@@ -30,18 +31,46 @@ class CSV2JSON(object):
 
     #Function to get json format from csv format
 
+  def nestedJSON(self, csv_list):
+
+    nested_json = []
+
+    data = sorted(csv_list, key=lambda r: r[1])
+    
+    for key, group in itertools.groupby(csv_list, key=lambda r: r[0]):
+      group_rows = [row[1:] for row in group]
+      
+      if len(row[1:]) == 1:
+        nested_json.append({"name": row[0],"size": int(row[1])})
+      else:
+        nested_json.append({"name": key,"children":self.nestedJSON(group_rows)})
+
+    return nested_json 
+   
+  def getNestedJSON(self):
+
+    csvfile = open(self.csv_file, 'r')
+    jsonfile = open(self.json_file, 'w')
+
+    self.header = csvfile.readline().strip().split(",")
+    #reading csv as list of list
+    csv_list = list(csv.reader(csvfile))
+    
+    json_out = json.dumps(self.nestedJSON(csv_list),indent = 2)
+    jsonfile.write(json_out)
+
   def getJSON(self):
     
       csvfile = open(self.csv_file, 'r')
       jsonfile = open(self.json_file, 'w')
 
-      fieldnames = csvfile.readline().strip().split(",")
-      print fieldnames
+      self.header = csvfile.readline().strip().split(",")
 
-      read_csv = csv.DictReader(csvfile, fieldnames)
+      read_csv = csv.DictReader(csvfile, self.header)
       json_out = json.dumps( [ row for row in read_csv ])
       jsonfile.write(json_out) 
      
 if __name__ == "__main__":
   obj = CSV2JSON()
-  obj.getJSON()
+  #obj.getJSON()
+  obj.getNestedJSON()
